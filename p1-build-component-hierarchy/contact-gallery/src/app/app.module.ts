08ac9c767e1f33c2638a6e6efc7c0d904ff25748
Rishabh Gist <damyrachit@gmail.com>
2022-11-01 12:53:22 +0530

    added container

--- p1-build-component-hierarchy/contact-gallery/src/app/app.module.ts
+++ p1-build-component-hierarchy/contact-gallery/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { IconComponent } from './icon/icon.component';
 import { SearchComponent } from './search/search.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     IconComponent,
-    SearchComponent
+    SearchComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
