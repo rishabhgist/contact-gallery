c382205fdd05c7236dcd2676b494cbaa9e7cc815
Rishabh Gist <damyrachit@gmail.com>
2022-11-01 12:54:07 +0530

    addec card

--- p1-build-component-hierarchy/contact-gallery/src/app/app.module.ts
+++ p1-build-component-hierarchy/contact-gallery/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { IconComponent } from './icon/icon.component';
 import { SearchComponent } from './search/search.component';
 import { ContainerComponent } from './container/container.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ContainerComponent } from './container/container.component';
     HeaderComponent,
     IconComponent,
     SearchComponent,
-    ContainerComponent
+    ContainerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
