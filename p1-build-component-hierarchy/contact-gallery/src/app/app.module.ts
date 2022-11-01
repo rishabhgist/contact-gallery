8096427b5375e80a4d526ab6887c97249829af39
Rishabh Gist <damyrachit@gmail.com>
2022-11-01 12:51:56 +0530

    added icon

--- p1-build-component-hierarchy/contact-gallery/src/app/app.module.ts
+++ p1-build-component-hierarchy/contact-gallery/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IconComponent } from './icon/icon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule
