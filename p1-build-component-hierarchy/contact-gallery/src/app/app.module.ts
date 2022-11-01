e6bd24ed5bd8af7a530003f259181031886bf4ee
Rishabh Gist <damyrachit@gmail.com>
2022-11-01 12:52:20 +0530

    added search

--- p1-build-component-hierarchy/contact-gallery/src/app/app.module.ts
+++ p1-build-component-hierarchy/contact-gallery/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { IconComponent } from './icon/icon.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IconComponent
+    IconComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
